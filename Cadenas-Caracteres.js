8e3ba198a570ada8fa6cb2a6c86bed837ab93177
denpo29 <gonza.dp29@hotmail.com>
2023-02-22 11:08:36 -0300

    Agregando los archivos al repositorio del cuarto ejercicio

---
+++ Cadenas-Caracteres.js
@@ -0,0 +1,18 @@
+let nombre = "Denis"
+let apellido = "Ponce"
+let estudiante = nombre.concat(" ", apellido)
+console.log(estudiante)
+let estudianteMayus = estudiante.toUpperCase()
+console.log(estudianteMayus)
+let estudianteMinus = estudiante.toLowerCase()
+console.log(estudianteMinus)
+let total_letras= estudiante.length
+console.log(total_letras)
+let inicial = estudiante.charAt(0)
+console.log(inicial)
+let final = estudiante.charAt(10)
+console.log(final)
+let sin_espacios = estudiante.trim()
+console.log(sin_espacios)
+let nombre_esta = estudiante.includes("Denis")
+console.log (nombre_esta)
